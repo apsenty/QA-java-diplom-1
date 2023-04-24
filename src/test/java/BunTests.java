95f198e300e1f58ccd17372bf405946ee2fc6b23
Olga Polyakova <olgaleto@yandex.ru>
2023-04-24 21:55:26 +0600

    add BunTests.java and modify pom.xml

---
+++ src/test/java/BunTests.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import praktikum.Bun;
+import static org.junit.Assert.*;
+
+public class BunTests {
+    Bun bun = new Bun("TestName", 100);
+
+    @Test
+    public void getNameShouldReturnTestName() {
+        assertEquals("Значение должно быть TestName","TestName", bun.getName());
+    }
+
+    @Test
+    public void getPriceShouldReturn100() {
+        assertEquals("Значение должно быть 100",100, bun.getPrice(), 0);
+    }
+}
