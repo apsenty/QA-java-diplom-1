2e63a889536da6bec23ecb4b4b29fa9d93936de4
Olga Polyakova <olgaleto@yandex.ru>
2023-05-05 20:54:25 +0600

    fix after review

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -3,15 +3,87 @@ import praktikum.Bun;
 import static org.junit.Assert.*;
 
 public class BunTest {
-    Bun bun = new Bun("TestName", 100);
+
 
     @Test
     public void getNameShouldReturnTestName() {
+        Bun bun = new Bun("TestName", 100);
         assertEquals("Значение должно быть TestName","TestName", bun.getName());
     }
 
     @Test
+    public void getNameNullNameShouldReturnNull() {
+        Bun bun = new Bun(null, 100);
+        assertNull("Значение должно быть null", bun.getName());
+    }
+
+    @Test
+    public void getNameEmptyNameShouldReturnEmpty() {
+        Bun bun = new Bun("", 100);
+        assertEquals("Должна вернуться пустая строка", "", bun.getName());
+    }
+
+    @Test
+    public void getNameNameWithSpacesShouldReturnCorrectly() {
+        String expectedName = "Тестовая булочка с пробелами";
+        Bun bun = new Bun("Тестовая булочка с пробелами", 100);
+        assertEquals("Значение должно быть \"Тестовая булочка с пробелами\"", expectedName, bun.getName());
+    }
+
+    @Test
+    public void getNameTooLongNameWithNumbersShouldReturnCorrectly() {
+        String expectedName = "7FXWbИnИТЛxхкzвлщ1цafd9гEЮGzЧLвlхwВqЗЛй49ФЦткхgyх4ОjА5Ъ3МКЙнwCUKс61hVкОk" +
+                "IырЧ2DIZъю8eшAiTyцN8UIуХТЪ7ХныйЯю2цTйmККьпЩБйюPWтoЯ9пкuБcДябъГМqvХЧбQ0HхmFаюМFnмxшUаЖФИB" +
+                "ч3ШЖ7ut6ХщGЕгU0яHКяDЖ2dDiОСfЛdЦzCрЬBкuGРЛЮПлфЮЫЛнмpHнАгHOйзОЧSвk4дЪрOmЮgGг0SBРsтовGлxРЗh" +
+                "ъSэ8вЕФdXЪмьwcчНЗм6ЭфdDх0IьcьsюGUэГоЗ5лЭzмйБоMсoXЩclREиXЙsОHшOзkтlЙпLГйwЦъL3Е1цmЫкxЮКfYР" +
+                "HyнpWrХAEaИтл8GяhжvРьrтшЩйЛgйakх2PчгвzНБшЩоGelДжnЪРy1ПGXUУтгEGпщЙьЩлgqzaYMhуWApшКvЦKCПОM" +
+                "ьКiLPАЧeсДeБ3sЩ8YРФfGяшхЬНСкlЫpзXOчф8зщkq0дСwndGкdmБжRmМkлrЕUCОOалсYB6ЕТlш6cяMHчdWyЛZ3fЛ" +
+                "aъoЪqАlAдVГчXnамфe4пJKВOWБSМфlC82БH0iiRкfи6GiNЦExPVfЩСEfБ28ФЦтчжbVwBлГсСД130Qht0в5payFУа" +
+                "а2ВоyлаДacГЭфtКдyxхlЕiЧoРAбkНNZЩDЖыA0УШИУpzCgGaYЖс5FыыgАСЛegеsщ1йЖjЖЩmyнgРТxr4хrтЗKщкУйY" +
+                "fPХрJreЫцaыeуy3LаR5КlЛUфOQqSPZщDD5БИДа0зgм4m3UGeзрОытDсыФЙHИ0AзtЩ6RСцЗйчsЛсSьhуеоРг7зЩMс" +
+                "oPDfЦоАC9ЗdсАvЖxXОЧIqЪwюWЕVОaЮшиoйbИФЬEwрJдгwхЧЬn3hХSяiЭkTжоюфУБдЖbУ4Jэ7xiXзАVOИ9Eя9ВЭЛe" +
+                "ГzлрQvZькЯЙж47WМeокPT4bdэХыцjюcФtTям";
+        Bun bun = new Bun(expectedName, 100);
+        assertEquals("Значение должно соответствовать переменной expectedName", expectedName, bun.getName());
+    }
+
+    @Test
+    public void getNameNameWithSpecSymbolsShouldReturnCorrectly() {
+        String expectedName = "uWs.jisy!vE%ziQBK&KnqnddEo@cTK*S@PHuG@pz=ZiEOQ%(GSkE@FWPI!XN-K!gHCbzXMkP(X" +
+                "ncigfXMNaImFuqEeAiA=jCP@pL";
+        Bun bun = new Bun(expectedName, 100);
+        assertEquals("Значение должно соответствовать переменной expectedName", expectedName, bun.getName());
+    }
+
+    @Test
     public void getPriceShouldReturn100() {
+        Bun bun = new Bun("TestName", 100);
         assertEquals("Значение должно быть 100",100, bun.getPrice(), 0);
+    }
+
+    @Test
+    public void getPriceDecimalPriceShouldReturnCorrectly() {
+        Bun bun = new Bun("Test bun", 10.34F);
+        assertEquals("Значение должно быть 10.34", 10.34F, bun.getPrice(), 0);
+    }
+
+    @Test
+    public void getPriceMaxNegativeNumberShouldReturnCorrectly() {
+        float expectedPrice = 1.175494351e-38F;
+        Bun bun = new Bun("Test bun", expectedPrice);
+        assertEquals("Значение должно быть -100", expectedPrice, bun.getPrice(), 0);
+    }
+
+    @Test
+    public void getPriceZeroPriceShouldReturnZero() {
+        Bun bun = new Bun("Test bun", 0);
+        assertEquals("Значение должно быть 0", 0, bun.getPrice(), 0);
+    }
+
+    @Test
+    public void getPriceMaxFloatPriceShouldReturnCorrectly() {
+        float expectedPrice = 3.402823466e+38F;
+        Bun bun = new Bun("Test bun", expectedPrice);
+        assertEquals("Значение должно соответствовать переменной expectedPrice", expectedPrice, bun.getPrice(), 0);
     }
 }
--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -1,24 +1,18 @@
 import org.junit.Test;
 import praktikum.*;
-import java.util.List;
 import static org.junit.Assert.*;
 
 public class BurgerTest {
     Burger burger = new Burger();
-    //Database database = new Database();
-    //List<Bun> buns = database.availableBuns();
-    //List<Ingredient> ingredients = database.availableIngredients();
 
     @Test
     public void addIngredientListShouldGrow() {
-        //burger.addIngredient(ingredients.get(0));
         burger.addIngredient(new Ingredient(IngredientType.FILLING, "test_ingredient", 100));
         assertEquals(1, burger.ingredients.size());
     }
 
     @Test
     public void removeIngredientListShouldGetSmaller() {
-        //burger.addIngredient(ingredients.get(0));
         burger.addIngredient(new Ingredient(IngredientType.FILLING, "test_ingredient", 100));
         burger.removeIngredient(0);
         assertEquals(0, burger.ingredients.size());
@@ -26,25 +20,10 @@ public class BurgerTest {
 
     @Test
     public void moveIngredientShouldChangeIndex() {
-        //burger.addIngredient(ingredients.get(0));//0
         burger.addIngredient(new Ingredient(IngredientType.FILLING, "test_ingredient", 100));
-        //Ingredient newIngredient = ingredients.get(1);
         Ingredient newIngredient = new Ingredient(IngredientType.SAUCE, "test_2_ingredient", 200);
         burger.addIngredient(newIngredient); //1
         burger.moveIngredient(1,0);
         assertEquals(0, burger.ingredients.indexOf(newIngredient));
     }
-
-    /* @Test
-    public void setBunsPositiveCheck() {
-        burger.setBuns(buns.get(1));
-        assertEquals("Значение должно быть white bun","white bun", burger.bun.getName());
-    }
-
-    @Test
-    public void getPricePositiveCheck() {
-        burger.setBuns(buns.get(0)); //100*2 = 200
-        burger.addIngredient(ingredients.get(0)); //100
-        assertEquals("Значение должно быть 300", 300, burger.getPrice(), 0);
-    } */
 }
